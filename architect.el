3fc84c1e29ddf2d100388ebd60e481bae1e08779
Marc-Antoine Loignon <developer@lognoz.org>
2020-04-28 03:57:30 -0400

    Architect: Add function to replacement template variables in file

--- architect.el
+++ architect.el
@@ -24,13 +24,27 @@
 
 (require 'subr-x)
 
-;;; Contextual Architect constants.
+;;; Contextual Architect constant.
 
 (defconst architect-base-directory (file-name-directory load-file-name)
   "The directory Architect was loaded from.")
 
-(defconst architect-source-directory (concat architect-base-directory "src/")
+;;; Contextual Architect variables.
+
+(defvar architect-source-directory (concat architect-base-directory "src/")
   "The directory of source templates.")
+
+(defvar architect-template-variables nil
+  "The list of template variables.")
+
+(defvar architect-template-replacements nil
+  "The list of template replatement.")
+
+(defvar architect-template-destination nil
+  "The destination directory template.")
+
+(defvar architect-template-default-directory nil
+  "The template default directory.")
 
 ;;; Internal Architect face.
 
@@ -52,87 +66,124 @@
           (push (file-name-nondirectory path) list))))
     list))
 
-(defun architect-template-recursive-source (path)
-  "This function return this list of recursive files and
-directories by path."
-  (split-string
-    (shell-command-to-string
-      (format "find %s" path) "\n" t)))
-
 (defun architect-load-configuration (path)
   "This function load project configuration by path."
-  (let ((path (concat path "/project.el")))
+  (let ((path (concat path "/template.el")))
     (if (file-exists-p path)
         (load path nil 'nomessage)
       (error (format "File '%s' not found." path)))))
 
-(defun architect-read-directory (target)
+(defun architect-define-variable (&rest args)
+  "Define variables for a template. It's generally used in
+`template.el' file."
+  (setq architect-template-variables
+        (append architect-template-variables (list args))))
+
+(defun architect-define-default-directory (directory)
+  "Define directory for a template. It's generally used in
+`template.el' file."
+  (when (file-directory-p directory)
+    (setq architect-template-default-directory directory)))
+
+(defun architect-plist-get (plist key)
+  "This function is used to get plist value."
+  (eval (plist-get plist key)))
+
+(defun architect-read-string (plist)
+  "This function is used to get user input string. It require an)
+non-empty string before to return it."
+  (let ((answer) (valid) (prompt-text) (prompt-error-text)
+        (input (architect-plist-get plist :input))
+        (input-error (architect-plist-get plist :input-error))
+        (regex (architect-plist-get plist :regex)))
+    (unless regex (setq regex ".+"))
+    (unless input-error (setq input-error "require"))
+    (setq prompt-text (concat input ": ")
+          prompt-error-text
+            (format "%s (%s): " input
+                    (propertize input-error 'face
+                                'architect-error-face)))
+    (while (not valid)
+      (setq answer (string-trim (read-string prompt-text answer)))
+      (if (string-match-p regex answer)
+          (setq valid t)
+        (setq prompt-text prompt-error-text)))
+    answer))
+
+(defun architect-set-directory (target)
   "This function provide a prompt input to ask where the project
 will be created."
-  (let* ((variable (format "architect-%s-default-directory" target))
-         (prompt-default-directory (architect-default-directory variable)))
-    (concat
-      (read-directory-name "Directory: " prompt-default-directory nil t)
-      (architect-read-string "Project directory"  "^[-_A-Za-z0-9]+$"  "alphanumeric") "/")))
+  (let* ((base-directory
+           (read-directory-name
+             "Directory: "
+             architect-template-default-directory
+             nil t))
+         (project-directory
+           (architect-read-string
+             '(:regex "^[-_A-Za-z0-9]+$"
+               :input (concat "Destination " base-directory)
+               :input-error "alphanumeric"))))
+    (setq architect-template-destination
+          (concat base-directory project-directory))))
 
-(defun architect-default-directory (variable-name)
-  "This function return the default directory defined variable)))))
-value. If it doesn't exists, it return none."
-  (let ((variable (intern variable-name))
-        (directory))
-    (when (boundp variable)
-      (setq variable (symbol-value variable))
-      (when (file-directory-p variable)
-        (setq directory variable)))
-    directory))
+(defun architect-set-variables ()
+  "This function loop into `architect-template-variables' and))))
+execute an user prompt. It return an assosiative array that will
+be used to replace in template file."
+  (let ((variables) (selector) (value))
+    (dolist (args architect-template-variables)
+      (setq selector (architect-plist-get args :variable))
+      (setq value (architect-read-string args))
+      (push (cons selector value) architect-template-replacements))))
 
-(defun architect-read-args (variable)
-  "This function loop into project variables and execute an user
-prompt. It return an assosiative array that will be used to
-replace in template file."
-  (let ((args))
-    (mapc
-      (lambda (entry)
-        (let* ((replacement (car entry))
-               (text (cadr entry))
-               (value))
-          (setq value
-            (if (equal (length entry) 4)
-                (architect-read-string text (caddr entry) (cadddr entry))
-              (architect-read-string text)))
-         (push (cons replacement value) args)))
-      (symbol-value (intern variable)))
-    args))
+(defun architect-recursive-directory ()
+  "This function return this list of recursive files and
+directories by path."
+  (split-string
+    (shell-command-to-string "find .")))
 
-(cl-defun architect-read-string (text &optional (regex ".+") (regex-error "require"))
-  "This function is used to get user input string. It require an
-non-empty string before to return it."
-  (let ((answer) (not-empty)
-        (input (format "%s: " text))
-        (input-error
-         (format "%s (%s): " text
-                 (propertize regex-error 'face
-                             'architect-error-face))))
-    (while (not not-empty)
-      (setq answer (string-trim (read-string input answer)))
-      (if (string-match-p regex answer)
-          (setq not-empty t)
-        (setq input input-error)))
-    answer))
+(defun architect-replace-filename (path)
+  "This function apply filename replacement based on template
+variables."
+  (let ((return-path path))
+    (dolist (replacement architect-template-replacements)
+      (let ((variable (car replacement))
+            (value (cdr replacement)))
+        (when (string-match-p variable path)
+          (setq return-path
+            (replace-regexp-in-string
+              variable value return-path nil 'literal))
+          (rename-file path return-path))))))
+
+(defun architect-apply-replacement (path)
+  "This function scan file content to apply replacement based on
+template variables."
+  (with-temp-file path
+    (insert-file-contents-literally path)
+    (dolist (replacement architect-template-replacements)
+      (goto-char 0)
+      (while (search-forward (car replacement) nil t)
+        (replace-match (cdr replacement)))
+      replacement)))
 
 (defun architect-create-project (template-name)
   "This function is used as action to ivy prompt executed in main
 `architect' function."
-  (let* ((directory) (args)
-         (path (concat architect-source-directory template-name))
-         (variable (format "architect-%s-variables" template-name)))
+  (let* ((path (concat architect-source-directory template-name)))
     (architect-load-configuration path)
-    (unless (boundp (intern variable))
-      (error (format "Variable '%s' not found." variable)))
-    (setq directory (architect-read-directory template-name))
-    (when (file-directory-p directory)
-      (error (format "Directory '%s' already exists." directory)))
-    (setq args (architect-read-args variable))))
+    (architect-set-directory template-name)
+    (architect-set-variables)
+    (shell-command (format "cp -r %s %s" path architect-template-destination))
+    (message (format "Creating %s..." architect-template-destination))
+    (let ((default-directory architect-template-destination))
+      (dolist (path (architect-recursive-directory))
+        (architect-replace-filename path))
+      (dolist (path (architect-recursive-directory))
+        (message (format "Checking %s for replacement..." path))
+        (unless (file-directory-p path)
+          (architect-apply-replacement path))))
+    (dired architect-template-destination)
+    (message "")))
 
 ;;; External Architect functions.
 
@@ -142,6 +193,9 @@ non-empty string before to return it."
 you want to create a new template, please add it to Architect src
 directory."
   (interactive)
+  (setq architect-template-variables nil
+        architect-template-replacements nil
+        architect-template-default-directory nil)
   (ivy-read "Create project: "
             (architect-template-candidates)
             :require-match t
