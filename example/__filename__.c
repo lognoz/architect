6457309b9f44f04d10cdb0eefd3213b59ce2933e
Marc-Antoine Loignon <developer@lognoz.org>
2020-04-30 13:53:07 -0400

    Example: Add date variable to architect template

--- example/__filename__.c
+++ example/__filename__.c
@@ -1,3 +1,5 @@
+/* This program was generated on __date__. */
+
 #include <stdio.h>
 
 int main() {
--- example/architect.el
+++ example/architect.el
@@ -3,6 +3,10 @@
 (architect-define-default-directory "~/")
 
 (architect-define-variable
+  :variable "__date__"
+  :value (format-time-string "%Y-%m-%d"))
+
+(architect-define-variable
   :variable "__filename__"
   :regex "^[-_A-Za-z0-9]+$"
   :input "Filename"
