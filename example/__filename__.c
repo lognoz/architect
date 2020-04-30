f2ea8c7aca17294f0a7aa75004b8ab39710469c4
Marc-Antoine Loignon <developer@lognoz.org>
2020-04-30 13:32:17 -0400

    Example: Add initial C program example

---
+++ example/__filename__.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("__print__");
+	return 0;
+}
---
+++ example/architect.el
@@ -0,0 +1,19 @@
+;; template.el --- Architect Template Example -*- lexical-binding: t; -*-
+
+(architect-define-default-directory "~/")
+
+(architect-define-variable
+  :variable "__filename__"
+  :regex "^[-_A-Za-z0-9]+$"
+  :input "Filename"
+  :input-error "alphanumeric"
+  :after-function 'downcase)
+
+(architect-define-variable
+  :variable "__print__"
+  :input "Print message"
+  :after-function 'capitalize)
+
+(architect-define-commit
+  :add "."
+  :message "Add initial commit")
