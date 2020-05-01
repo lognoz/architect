743eb274c3540a95207d26d27e26da7522a1842e
Marc-Antoine Loignon <developer@lognoz.org>
2020-05-01 13:04:57 -0400

    Make: Rephrase generate-autoload documentation

--- make/autoload.el
+++ make/autoload.el
@@ -1,7 +1,7 @@
 ;; autoload.el --- Architect Autoload -*- lexical-binding: t; -*-
 
 (defun generate-autoload (path)
-  "Generate autoload file by given PATH."
+  "Generate autoload file from given PATH."
   (let ((generated-autoload-file (expand-file-name "autoload.el")))
     (update-file-autoloads path t generated-autoload-file)))
 
