3e74b6f54f04ef92668e4a005bed7d78736dbc19
Marc-Antoine Loignon <developer@lognoz.org>
2020-05-04 21:29:27 -0400

    Test: Fix architect variable, commit and shell command definition

--- example/architect.el
+++ example/architect.el
@@ -1,23 +1,25 @@
 ;; template.el --- Architect Template Example -*- lexical-binding: t; -*-
 
-(architect-define-default-directory "~/")
+(architect-default-directory "~/document")
 
-(architect-define-variable
+(architect-variable
   :variable "__date__"
   :value (format-time-string "%Y-%m-%d"))
 
-(architect-define-variable
+(architect-variable
   :variable "__filename__"
-  :regex "^[-_A-Za-z0-9]+$"
+  :regex "^[-_a-z0-9]+$"
   :input "Filename"
-  :input-error "alphanumeric"
-  :after-function 'downcase)
+  :input-error "alphanumeric")
 
-(architect-define-variable
+(architect-variable
   :variable "__print__"
   :input "Print message"
   :after-function 'capitalize)
 
-(architect-define-commit
+(architect-commit
   :add "."
   :message "Add initial commit")
+
+(architect-shell-command
+  :command "make")
